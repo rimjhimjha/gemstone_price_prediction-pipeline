296624a8d414fe8f0faa0fabaea07a64fab1fa60
Rimjhim Jha <rimmijha2608@gmail.com>
2025-02-15 15:52:37 +0530

    logger and exception updated

---
+++ src/exception/exception.py
@@ -0,0 +1,22 @@
+import sys
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.filename = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occurred in Python script name [{0}] at line number [{1}]: {2}".format(
+            self.filename, self.lineno, str(self.error_message)
+        )
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        raise CustomException(e, sys)
