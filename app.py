b5638a8e29dd6e14c6fe25bfd93d67bf8f73906e
Rimjhim Jha <rimmijha2608@gmail.com>
2025-02-17 21:22:13 +0530

    app works fine

--- app.py
+++ app.py
@@ -12,27 +12,31 @@ def predict_datapoint():
     if request.method == "GET":
         return render_template("form.html")
     else:
-        data = CustomData(
-            carat=float(request.form.get("carat", 0.0)),
-            depth=float(request.form.get("depth", 0.0)),
-            table=float(request.form.get("table", 0.0)),
-            x=float(request.form.get("x", 0.0)),
-            y=float(request.form.get("y", 0.0)),
-            z=float(request.form.get("z", 0.0)),
-            cut=request.form.get("cut", ""),
-            color=request.form.get("color", ""),
-            clarity=request.form.get("clarity", ""),
-        )
+        try:
+            data = CustomData(
+                carat=float(request.form["carat"]),
+                depth=float(request.form["depth"]),
+                table=float(request.form["table"]),
+                x=float(request.form["x"]),
+                y=float(request.form["y"]),
+                z=float(request.form["z"]),
+                cut=request.form["cut"],
+                color=request.form["color"],
+                clarity=request.form["clarity"],
+            )
 
-        final_data = data.get_data_as_dataframe()
-        predict_pipeline = Predictpipeline()
-        prediction = predict_pipeline.predict(final_data)
+            final_data = data.get_data_as_dataframe()
+            predict_pipeline = Predictpipeline()
+            prediction = predict_pipeline.predict(final_data)
 
-        final_result = round(prediction[0], 2)
+            final_result = round(prediction[0], 2)
 
-        print(f"Prediction: {final_result}")  # Debugging statement
+            print(f"Prediction: {final_result}")  # Debugging statement
 
-        return render_template("result.html", final_result=final_result)
+            return render_template("result.html", final_result=final_result)
+
+        except ValueError as e:
+            return f"Error: {str(e)} - Invalid input detected. Please ensure all fields are filled correctly."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -20,8 +20,7 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 
 model_trainer_obj=ModelTrainer()
-model_trainer_obj.initiate_model_training(train_arr, test_arr)
-
+model_trainer_obj.initiate_model_training(train_arr,test_arr)
 
 model_eval_obj = ModelEvaluation()
 model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
